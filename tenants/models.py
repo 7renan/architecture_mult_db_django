18c61c7d60d50ce4e0d6233f1af822c2aa6c9e26
7renan <lima.renan96@gmail.com>
2021-07-16 09:01:23 -0300

    create models in tenants

--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tenant(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    domain_prefix = models.CharField('Prefixo do domínio', max_length=50)
+
+
+class TenantAware(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
