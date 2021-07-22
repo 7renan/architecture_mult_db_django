4e7cb08be06c201346a1be94f3118516648660fb
7renan <lima.renan96@gmail.com>
2021-07-22 15:12:03 -0300

    create core service get_tenants_map

--- core/core_services.py
+++ core/core_services.py
@@ -7,3 +7,12 @@ def host_from_request(request):
     prefix_domain = host_name.split('.')[0]
     tenant = Tenant.objects.filter(domain_prefix=prefix_domain).first()
     return tenant
+
+
+def get_tenants_map():
+    return {
+        "renan.tasks.localhost": "renan",
+        "steve.tasks.localhost": "steve",
+    }
+
+
